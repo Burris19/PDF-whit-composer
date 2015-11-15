09022eaa6424ef11fa3a8c97883fd7afd41590c4
Burris19 <julianhernandez19@gmail.com>
2015-11-15 20:13:25 +0000

    [add] install dompdf and example of use

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julian
+ * Date: 15/11/15
+ * Time: 01:50 PM
+ */
+
+// somewhere early in your project's loading, require the Composer autoloader
+// see: http://getcomposer.org/doc/00-intro.md
+require '../vendor/autoload.php';
+
+// disable DOMPDF's internal autoloader if you are using Composer
+define('DOMPDF_ENABLE_AUTOLOAD', false);
+
+// include DOMPDF's default configuration
+require_once '../vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+
+$html =
+    '<html><body>'.
+    '<h1>Julian Hernandez</h1>'.
+    '<p>Aprendiendo Composer en el Curso de Laravel 5.</p>'.
+    '</body></html>';
+
+$dompdf = new DOMPDF();
+$dompdf->load_html($html);
+$dompdf->render();
+$dompdf->stream("sample.pdf");
+
+?>
